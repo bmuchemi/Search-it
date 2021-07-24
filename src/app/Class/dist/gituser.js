02d14951b5aede5c1d95c1d7efa0747ced655ade
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-24 18:18:38 +0300

    add content to git user class

---
+++ src/app/Class/dist/gituser.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Gituser = void 0;
+var Gituser = /** @class */ (function () {
+    function Gituser() {
+    }
+    return Gituser;
+}());
+exports.Gituser = Gituser;
--- src/app/Class/gituser.ts
+++ src/app/Class/gituser.ts
@@ -1,2 +1,8 @@
 export class Gituser {
+    public name:string;
+    public html_url:string; 
+    public location:string;
+    public public_repos:number;
+    public followers:number;
+    public following:number;
 }
